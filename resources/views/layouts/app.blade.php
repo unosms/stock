98a624007eef564e17dad69331f96e09deb59eb4
Walid Alassaad <unosms.net@gmail.com>
2026-03-01 23:38:28 +0200

    Set browser tab title to Stock Manager across app and login

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Stock Manager') }}</title>
+        <title>Stock Manager</title>
 
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600,700,800&display=swap" rel="stylesheet" />
@@ -47,4 +47,3 @@
         </div>
     </body>
 </html>
-
